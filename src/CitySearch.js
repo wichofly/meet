3dd2bb07d2801ae3de92c4acc19bebe99f81e7c6
Mauricio Erazo <antonierazo13@hotmail.com>
2022-08-30 16:35:11 +0200

    Scenario 2 all green light!

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 
 class CitySearch extends Component {
+  state = {
+    query: '',
+    suggestions: [],
+  }
+
+  // The state needs to be changed whenever the textbox changes. 
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    const suggestions = this.props.locations.filter((location) => {
+      return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+    });
+    this.setState({
+      query: value,
+      suggestions,
+    });
+  };
+
   render() {
     return (
       <div className='CitySearch'>
-        <input type='text' className='city' />
-        <ul className='suggestions'>
+        <input
+          type="text"
+          className="city"
+          value={this.state.query}
+          // detect whether any textual changes have been made on the input:
+          onChange={this.handleInputChanged}
+        />
+        <ul className="suggestions">
+          {this.state.suggestions.map((suggestion) => (
+            <li key={suggestion}>{suggestion}</li>
+          ))}
+          <li key='all'>
+            <b>See all cities</b>
+          </li>
         </ul>
       </div>
     );
