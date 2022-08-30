7d323cd4e1a3ece3d263ae1d1deb21e1651f792c
Mauricio Erazo <antonierazo13@hotmail.com>
2022-08-30 12:52:40 +0200

    Sceario 2, pass

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,10 +3,16 @@ import { shallow } from 'enzyme';
 
 import App from '../App';
 import EventList from '../EventList';
+import CitySearch from '../CitySearch';
 
 describe('<App /> component', () => {
   test('render list of events', () => {
     const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
+
+  test('render CitySearch', () => {
+    const AppWrapper = shallow(<App />);
+    expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
 });
