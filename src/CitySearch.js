def6e4648eab47a251a62d2ee1107fd91e3e3f39
Mauricio Erazo <antonierazo13@hotmail.com>
2022-08-30 13:17:31 +0200

    Optimize other test files and adding input to search file

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -4,6 +4,9 @@ class CitySearch extends Component {
   render() {
     return (
       <div className='CitySearch'>
+        <input type='text' className='city' />
+        <ul className='suggestions'>
+        </ul>
       </div>
     );
   }
