5fc90865fd8502999f2b5f0aa4f1513b5b05a0fb
Mauricio Erazo <antonierazo13@hotmail.com>
2022-08-30 12:37:34 +0200

    Secenario 1, test pass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> You can do it Wicho!</h1>
-    </div>
-  );
+import './App.css';
+import EventList from './EventList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <EventList />
+      </div>
+    );
+  }
 }
 
 export default App;
