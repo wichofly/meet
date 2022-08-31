ff2589dde5479575032ecb63cde57b69a89d4dbd
Mauricio Erazo <antonierazo13@hotmail.com>
2022-08-31 19:20:47 +0200

    integrate the Google Calendar API into the project

--- src/api.js
+++ src/api.js
@@ -1,5 +1,8 @@
 import { mockData } from "./mock-data";
 
+import axios from 'axios';
+import NProgress from 'nprogress';
+
 /**
  *
  * @param {*} events:
@@ -14,6 +17,105 @@ export const extractLocations = (events) => {
   return locations;
 };
 
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
+
+  return result;
+};
+
+// this function does is check whether there’s a path, then build the URL with the current path (or build the URL without a path using window.history.pushState()).
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
+
+// written with the try…catch statement.
+const getToken = async (code) => {
+  try {
+    const encodeCode = encodeURIComponent(code);
+
+    const response = await fetch('https://cccdyv5sxf.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem("access_token", access_token);
+    return access_token;
+  } catch (error) {
+    error.json();
+  }
+}
+
+// const getToken = async (code) => {
+//   const encodeCode = encodeURIComponent(code);
+//   const { access_token } = await fetch(
+//     'https://cccdyv5sxf.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+//   )
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .catch((error) => error);
+
+//   access_token && localStorage.setItem("access_token", access_token);
+
+//   return access_token;
+// };
+
 export const getEvents = async () => {
-  return mockData
+  NProgress.start();
+
+  // if you’re using localhost, you return the mock data; otherwise, you use the real API.
+  if (window.location.href.startsWith("http://localhost")) {
+    NProgress.done();
+    return mockData;
+  }
+
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url = 'https://cccdyv5sxf.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
+    const result = await axios.get(url);
+    if (result.data) {
+      var locations = extractLocations(result.data.events);
+      localStorage.setItem("lastEvents", JSON.stringify(result.data));
+      localStorage.setItem("locations", JSON.stringify(locations));
+    }
+    NProgress.done();
+    return result.data.events;
+  }
+};
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const results = await axios.get(
+        "https://cccdyv5sxf.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
 }
